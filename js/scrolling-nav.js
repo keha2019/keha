51b6ad8606c099614749ceeb58ed3a2422315101
huha123 <huha3213@gmail.com>
2020-01-02 13:51:11 +0900

    commit

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 56)
+          scrollTop: (target.offset().top - 100)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -23,7 +23,8 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 56
+    // offset: 56
+    offset: 100
   });
 
 })(jQuery); // End of use strict
